da58320f94e336826f850a88baf88e4f15c520ff
matheusknaul <contatomatheush0909@gmail.com>
2025-06-24 00:32:07 -0300

    git initialize

---
+++ src/main/java/com/matheusknaul/chefmanager/ChefManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matheusknaul.chefmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefManagerApplication.class, args);
+	}
+
+}
