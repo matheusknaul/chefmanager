273db0c1646c95934b79d7a0ee73cf778952789f
matheusknaul <contatomatheush0909@gmail.com>
2025-06-27 01:15:19 -0300

    feat: connection with MySQL and register() method in auth.py

--- mvp/app/routes/auth.py
+++ mvp/app/routes/auth.py
@@ -1,4 +1,6 @@
-from flask import url_for, jsonify, Blueprint
+from flask import url_for, jsonify, Blueprint, request
+from connection import conn
+from werkzeug.security import check_password_hash
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -9,3 +11,35 @@ def login():
             "message": "Login"
         }, 200
     )
+
+@auth_bp.route('/register', method=['POST'])
+def register():
+
+    data = request.json
+
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute(f'INSERT INTO user (name, password, email) values ({name}, {password}, {email});')
+
+    signal = cursor.fetchone()
+
+    id_new_user = cursor.execute(f'SELECT id FROM user WHERE name, email LIKE {name}, {email};')
+
+    
+
+    if signal:
+        return jsonify(
+            {
+                "message": "Usuário cadastrado com sucesso!",
+                "usuário":{
+
+                }
+            }, 201
+        )
+
+
+    return "a"
