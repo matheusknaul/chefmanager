7e4264d9dd6d7964d37c3b3abf6a95aa9d7bb4b1
matheusknaul <contatomatheush0909@gmail.com>
2025-07-01 23:09:19 -0300

    feat (backend): import unit route and ingredient route in __init__

--- mvp/app/__init__.py
+++ mvp/app/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from app.routes import auth_bp, users_bp, recipes_bp, orders_bp
+    from app.routes import auth_bp, users_bp, recipes_bp, orders_bp, ingredients_bp, units_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(users_bp, url_prefix='/users')
     app.register_blueprint(recipes_bp, url_prefix='/recipes')
     app.register_blueprint(orders_bp, url_prefix='/orders')
+    app.register_blueprint(ingredients_bp, url_prefix='/ingredients')
+    app.register_blueprint(units_bp, url_prefix='/units')
     
     return app
--- mvp/app/routes/__init__.py
+++ mvp/app/routes/__init__.py
@@ -2,3 +2,5 @@ from .auth import auth_bp
 from .user import users_bp
 from .recipes import recipes_bp
 from .order import orders_bp
+from .ingredient import ingredients_bp
+from .unit import units_bp
