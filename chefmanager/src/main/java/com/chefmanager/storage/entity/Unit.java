e70dfce59e52c64ddacfd5b71eeb1a3d664a24e6
matheusknaul <contatomatheush0909@gmail.com>
2025-07-06 18:46:57 -0300

    feat (backend): implemented methods and features in Unit Class

--- chefmanager/src/main/java/com/chefmanager/storage/entity/Unit.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/Unit.java
@@ -1,4 +1,117 @@
 package com.chefmanager.storage.entity;
 
-public class Unit {
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "Units")
+public class Unit extends BaseEntity{
+
+    @Id
+    private Integer id;
+
+    /**
+     * Title is the title of the unit, example: [Kilogram]
+     * */
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    /**
+     * AbTitle is the abbreviation of unit according to SI, example: [kg].
+     * */
+
+    @Column(nullable = false)
+    private String abTitle;
+
+    /**
+     * UnitType is the greatness of unity, example: [mass]
+     * */
+
+    @Column(nullable = false)
+    private String unitType;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @OneToMany(mappedBy = "ingredient")
+    @Column(nullable = false)
+    private Set<Ingredient> ingredients = new HashSet<>();
+
+
+    protected Unit(){
+
+    }
+
+    protected Unit(String title, String abTitle, String unitType){
+        this.setTitle(title);
+        this.setAbTitle(abTitle);
+        this.setUnitType(unitType);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbTitle() {
+        return abTitle;
+    }
+
+    public void setAbTitle(String abTitle) {
+        this.abTitle = abTitle;
+    }
+
+    public String getUnitType() {
+        return unitType;
+    }
+
+    public void setUnitType(String unitType) {
+        this.unitType = unitType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        } else if (this == other) {
+            return true;
+        } else if (this.getClass() == other.getClass()) {
+            Unit otherUnit = (Unit) other;
+            return Objects.equals(this.getId(), otherUnit.getId());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getId().hashCode();
+    }
 }
