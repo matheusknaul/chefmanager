517dc6431bc5abf56c25b68828518454f817c36e
matheusknaul <contatomatheush0909@gmail.com>
2025-07-06 21:58:18 -0300

    feat (backend): implements IngriendtStock class

--- chefmanager/src/main/java/com/chefmanager/storage/entity/IngredientStock.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/IngredientStock.java
@@ -1,6 +1,56 @@
 package com.chefmanager.storage.entity;
 
 
+import jakarta.persistence.*;
 
-public class IngredientStock {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class IngredientStock extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, updatable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "kitchenId", nullable = false)
+    private Kitchen kitchen;
+
+    @OneToMany(mappedBy = "stock")
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    @OneToMany(mappedBy = "unit")
+    private Set<Unit> units = new HashSet<>();
+
+    protected IngredientStock(){}
+
+    protected IngredientStock(Kitchen kitchen){
+        this.setKitchen(kitchen);
+        this.setName(kitchen.getName() + "'s stock");
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Kitchen getKitchen() {
+        return kitchen;
+    }
+
+    public void setKitchen(Kitchen kitchen) {
+        this.kitchen = kitchen;
+    }
 }
