815ee3c83bd6c6ddb88c79afe3c19db090a361c4
matheusknaul <contatomatheush0909@gmail.com>
2025-07-06 21:20:50 -0300

    feat: implements feats and methods in Ingredient Class

--- chefmanager/src/main/java/com/chefmanager/storage/entity/Ingredient.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/Ingredient.java
@@ -1,4 +1,104 @@
 package com.chefmanager.storage.entity;
 
-public class Ingredient {
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Ingredients")
+public class Ingredient extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "stockId")
+    private IngredientStock stock;
+
+    @ManyToOne
+    @JoinColumn(name = "unitId")
+    private Unit unit;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    protected Ingredient(){}
+
+
+    protected Ingredient(String name, Unit unit){
+        this.setName(name);
+        this.setUnit(unit);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public IngredientStock getStock() {
+        return stock;
+    }
+
+    public void setStock(IngredientStock stock) {
+        this.stock = stock;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        } else if (this == other) {
+            return true;
+        } else if (this.getClass() == other.getClass()) {
+            Ingredient otherIngredient = (Ingredient) other;
+            return Objects.equals(this.getId(), otherIngredient.getId());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getId().hashCode();
+    }
 }
