df24e9d4adb5253a977eb203b25e7506408601fc
matheusknaul <contatomatheush0909@gmail.com>
2025-07-09 22:25:39 -0300

    doc: add comment in readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Chef Management System
 
 The goal of this project is to create a (first) an MVP in Python, using the Flask Framework to speed up creation of the project and validate the idea.
+
+Initially, a REST API based on entities will be created and then a REST API based on actions will be created.
--- chefmanager/src/main/java/com/chefmanager/storage/entity/RecipeNote.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/RecipeNote.java
@@ -1,6 +1,7 @@
 package com.chefmanager.storage.entity;
 
 import com.chefmanager.common.datatransfer.RecipeNoteStatus;
+import com.chefmanager.common.util.StringDocTopic;
 import jakarta.persistence.*;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -20,14 +21,20 @@ public class RecipeNote extends BaseEntity{
     @ManyToOne
     private Recipe recipe;
 
-    @Column(nullable = false)
-    private Integer preparationTimeInMinutes;
+//    @Column(nullable = false)
+//    private Integer preparationTimeInMinutes;
+//
+//    @Column(nullable = false)
+//    private String preparationInstructions;
+//
+//    @Column(nullable = false)
+//    private String finalizationInstructions;
+//
+//    @Column(nullable = false)
+//    private String observations;
 
-    private String preparationInstructions;
-
-    private String finalizationInstructions;
-
-    private String observations;
+    @Column(columnDefinition = "TEXT")
+    private StringDocTopic instructions;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -35,6 +42,64 @@ public class RecipeNote extends BaseEntity{
 
     @UpdateTimestamp
     private Instant updateAt;
+
+    public RecipeNote() {
+    }
+
+    public RecipeNote(String title, Recipe recipe) {
+        this.setTitle(title);
+        this.setRecipe(recipe);
+    }
+
+    public RecipeNote(String title, StringDocTopic instructions, Recipe recipe) {
+        this.setTitle(title);
+        this.setInstructions(instructions);
+        this.setRecipe(recipe);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public StringDocTopic getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(StringDocTopic instructions) {
+        this.instructions = instructions;
+    }
+
+    public RecipeNoteStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RecipeNoteStatus status) {
+        this.status = status;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Instant updateAt) {
+        this.updateAt = updateAt;
+    }
 
 
 }
