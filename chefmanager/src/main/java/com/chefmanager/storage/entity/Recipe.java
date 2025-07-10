0377c081e03b38a7d5aff335f1f4a440f8c4c0ce
matheusknaul <contatomatheush0909@gmail.com>
2025-07-09 22:51:28 -0300

    feat (backend): implement methods in Recipe class

--- chefmanager/src/main/java/com/chefmanager/storage/entity/Recipe.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/Recipe.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Recipes")
@@ -25,6 +26,61 @@ public class Recipe extends BaseEntity{
     @ManyToOne
     private User user;
 
+    protected Recipe() {
+    }
 
+    protected Recipe(String title, List<RecipeNote> notes, List<RecipeIngredient> ingredients, User user) {
+        this.setTitle(title);
+        this.setNotes(notes);
+        this.setIngredients(ingredients);
+        this.setUser(user);
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<RecipeNote> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<RecipeNote> notes) {
+        this.notes = notes;
+    }
+
+    public List<RecipeIngredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<RecipeIngredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipe recipe = (Recipe) o;
+        return Objects.equals(id, recipe.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getId().hashCode();
+    }
 }
