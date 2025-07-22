b79446af89ecf387a122e40561fff3854cbc8145
matheusknaul <contatomatheush0909@gmail.com>
2025-07-21 22:20:22 -0300

    feat(backend): implemented SalesOrder and create 'productListItensToString()' method in SalesOrder to store all products in DB #3

--- chefmanager/src/main/java/com/chefmanager/storage/entity/SalesOrder.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/SalesOrder.java
@@ -15,7 +15,46 @@ public class SalesOrder extends Order{
 
     private List<Product> productList;
 
+    protected SalesOrder() {}
 
+    public SalesOrder(String productsString){this.setProductsString(productsString);}
 
+    public SalesOrder(List<Product> productList){ this.setProductsString(this.productListItensToString(productList));}
 
+    private String productListItensToString(List<Product> productList){
+        StringBuilder productCollection = new StringBuilder();
+        int productCollectionSize = productList.size();
+
+        productCollection.append("{\"products\": [");
+        for(int i = 0; i  < productCollectionSize; i++){
+            String recipes = productList.get(i).getRecipeCollection();
+            String productRecipes = recipes.substring(1, recipes.length() - 1);
+            productCollection.append("{\"id\": \"" + productList.get(i).getId() + "\", \"title\": \""+
+                    productList.get(i).getTitle() + "\", \"description\": \"" +
+                    productList.get(i).getDescription() + "\", " +
+                    productRecipes + "}");
+            if(i < productCollectionSize - 1){
+                productCollection.append(",");
+            }
+        }
+        productCollection.append("]}");
+
+        return productCollection.toString();
+    }
+
+    public String getProductsString() {
+        return productsString;
+    }
+
+    public void setProductsString(String productsString) {
+        this.productsString = productsString;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }
