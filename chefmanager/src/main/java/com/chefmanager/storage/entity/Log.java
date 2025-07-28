8649373c7b13fb2ad6b1b46e95c716a211cc608f
matheusknaul <contatomatheush0909@gmail.com>
2025-07-27 23:38:41 -0300

    feat: create Log class (basic) #2

--- chefmanager/src/main/java/com/chefmanager/storage/entity/Log.java
+++ chefmanager/src/main/java/com/chefmanager/storage/entity/Log.java
@@ -1,4 +1,89 @@
 package com.chefmanager.storage.entity;
 
+import com.chefmanager.common.datatransfer.LogLevel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Logs")
 public class Log extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LogLevel logLevel;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    @Column(columnDefinition = "TEXT")
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "kitchen_id")
+    private Kitchen kitchen;
+
+    protected Log() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(LogLevel logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Kitchen getKitchen() {
+        return kitchen;
+    }
+
+    public void setKitchen(Kitchen kitchen) {
+        this.kitchen = kitchen;
+    }
 }
